226b63d5fc1dee56231cbb46ab0f5b46c289dd03
nguyentito10 <tientoan91002003@gmail.com>
2024-11-18 08:39:52 +0700

    Update: Adding test code for mqtt and i2c

--- main/main.c
+++ main/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "mqtt_tcp.h"
 
+#define TEST_MQTT
+
 void app_main(void)
 {
-  mqtt_main();
+  #ifdef TEST_MQTT
+    mqtt_main();
+  #endif
+
+  #ifdef TEST_I2C
+    // do sth
+  #endif
+
 }
