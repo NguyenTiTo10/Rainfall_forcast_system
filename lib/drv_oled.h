b7b3283076572d18bfa1b871cf821934b83a9c1c
nguyentito10 <tientoan91002003@gmail.com>
2024-11-18 23:19:54 +0700

    Update: Creating header drv_oled.h

---
+++ lib/drv_oled.h
@@ -0,0 +1,15 @@
+#ifndef DRV_OLED_H
+#define DRV_OLED_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+/**
+ * @brief Initializes the OLED display.
+ * @param None
+ * @return None
+ */
+void oled_i2c_test(void);
+
+#endif // DRV_OLED_H
