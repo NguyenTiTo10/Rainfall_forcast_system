06ef79193c5a15fb6fc42106f614994483e6ffe2
nguyentito10 <tientoan91002003@gmail.com>
2024-11-29 14:21:02 +0700

    Move all the code from main to system manager

--- main/main.c
+++ main/main.c
@@ -2,6 +2,8 @@
 #include "driver/i2c.h"
 #include "esp_err.h"
 
+#include "system_manager.h"
+
 
 
 // I2C configuration
@@ -35,43 +37,7 @@ void app_main(void)
     else 
         printf("Failed to initialize I2C.\n");
 
-    if (drv_sh1106_init() == ESP_OK)
-        printf("Oled SH1106 initialized successfully.\n");
-
-    drv_sh1106_clear_screen();
-
-// #define DISPLAY_TEXT
-#ifdef DISPLAY_TEXT
-    // Test displaying text on the OLED
-    drv_sh1106_write_string(0, 0, "Hello, ESP-IDF!");
-    drv_sh1106_write_string(0, 1, "Multiline OK!");
-    drv_sh1106_write_string(0, 2, "ANOTHER LINE 2");
-    drv_sh1106_write_string(0, 3, "ANOTHER LINE 3");
-    drv_sh1106_write_string(0, 4, "ANOTHER LINE 4");
-    drv_sh1106_write_string(0, 5, "ANOTHER LINE 5");
-    drv_sh1106_write_string(0, 6, "ANOTHER LINE 6");
-    drv_sh1106_write_string(0, 7, "ANOTHER LINE 7");
-
-    bsp_delay (3000);
-
-#else
-    drv_sh1106_display_image(image_logo_uit);
-
-    bsp_delay (3000);
-
-    drv_sh1106_clear_screen();
-
-    bsp_delay (3000);
-
-    drv_sh1106_display_image(image_logo_ce);
-
-    bsp_delay (3000);
-
-#endif
-
-    // drv_sh1106_clear_screen();
-
-    drv_sh1106_turn_off();
+    void system_manage_loop();    
 
 #ifdef TEST_MQTT
   mqtt_main();
