48dbecd95c807ef04a2d3863db0657373bc9d033
nguyentito10 <tientoan91002003@gmail.com>
2024-12-02 14:57:37 +0700

    Adding bsp gpio files

--- lib/inc/bsp/bsp_timer.h
+++ lib/inc/bsp/bsp_timer.h
@@ -2,9 +2,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "stdint.h"
+#include <rom/ets_sys.h>
+#include "esp_timer.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #ifndef BSP_TIMER_H
 #define BSP_TIMER_H
 
 void bsp_delay (uint32_t time);
+void bsp_timer_ets_delay_us (uint32_t time_us);
+
 
 #endif
