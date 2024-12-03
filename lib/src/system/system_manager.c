6341ae0ad5b9c7b93d283fefaae9d30cd3a6f317
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 13:38:19 +0700

    Update: remove header image

--- lib/src/system/system_manager.c
+++ lib/src/system/system_manager.c
@@ -1,7 +1,8 @@
 #include "system_manager.h"
-#include "image_128x64.h"
 
-// static void system_oled_test_default ();
+
+
+static void system_oled_test_default ();
 static void system_oled_test_screen_1();
 
 static system_main_state_t current_state = IDLE;
@@ -35,83 +36,83 @@ esp_err_t system_manage_init (void)
 
 
 
-static void system_oled_test_default ()
-{
-  drv_sh1106_display_image(image_logo_uit);
+// static void system_oled_test_default ()
+// {
+//   drv_sh1106_display_image(image_logo_uit);
 
-  bsp_timer_delay (3000);
+//   bsp_timer_delay (3000);
 
-  drv_sh1106_clear_screen();
+//   drv_sh1106_clear_screen();
 
-  bsp_timer_delay (3000);
+//   bsp_timer_delay (3000);
 
-  drv_sh1106_display_image(image_logo_ce);
+//   drv_sh1106_display_image(image_logo_ce);
 
-  bsp_timer_delay (3000);
+//   bsp_timer_delay (3000);
 
-  drv_sh1106_clear_screen();
+//   drv_sh1106_clear_screen();
 
-  drv_sh1106_turn_off();
-}
-
-
-static void system_oled_test_screen_1()
-{
-  char date_time [] = "29.11.2024 - 19:00";
-  char location_1[] = "QUANG BINH";
-  char location_2[] = "HO CHI MINH";
-
-  drv_sh1106_draw_border_top(15, 0, 105, 18, 1);
-
-  drv_sh1106_display_text_center(0, date_time);
-
-  char temp[] = "Temp: 30.6 C ";
-  char humid[] = "Humid: 80 %";
-  char air_press[] = "Air press: 20 Pa";
+//   drv_sh1106_turn_off();
+// }
 
 
-  drv_sh1106_display_text(0, 3, temp);
-  drv_sh1106_display_text(0, 5, humid);
-  drv_sh1106_display_text(0, 7, air_press);
+// static void system_oled_test_screen_1()
+// {
+//   char date_time [] = "29.11.2024 - 19:00";
+//   char location_1[] = "QUANG BINH";
+//   char location_2[] = "HO CHI MINH";
+
+//   drv_sh1106_draw_border_top(15, 0, 105, 18, 1);
+
+//   drv_sh1106_display_text_center(0, date_time);
+
+//   char temp[] = "Temp: 30.6 C ";
+//   char humid[] = "Humid: 80 %";
+//   char air_press[] = "Air press: 20 Pa";
+
+
+//   drv_sh1106_display_text(0, 3, temp);
+//   drv_sh1106_display_text(0, 5, humid);
+//   drv_sh1106_display_text(0, 7, air_press);
 
   
-  drv_sh1106_display_text_center(1, location_1);
-  bsp_timer_delay (3000);
-  drv_sh1106_display_text_center(1, location_2);
+//   drv_sh1106_display_text_center(1, location_1);
+//   bsp_timer_delay (3000);
+//   drv_sh1106_display_text_center(1, location_2);
 
 
-}
+// }
 
 
-static void system_test_dht11 (void)
-{
-    uint32_t count = 0;
-    float temp = 0.0f, humid = 0.0f;  // Initialize variables to default values
+// static void system_test_dht11 (void)
+// {
+//     uint32_t count = 0;
+//     float temp = 0.0f, humid = 0.0f;  // Initialize variables to default values
 
 
-    while(1)
-    {
+//     while(1)
+//     {
       
-      if(drv_dht11_start_read())
-      {  
-        humid = drv_dht11_get_humid();
-        temp = drv_dht11_get_temp();
-        count += 1;
-      }
+//       if(drv_dht11_start_read())
+//       {  
+//         humid = drv_dht11_get_humid();
+//         temp = drv_dht11_get_temp();
+//         count += 1;
+//       }
 
-      printf("Count: %lu\n", count);
-      printf("[Temperature]> %.2f  \n",temp);
-      printf("[Humidity]> %.2f \n \n",humid);
+//       printf("Count: %lu\n", count);
+//       printf("[Temperature]> %.2f  \n",temp);
+//       printf("[Humidity]> %.2f \n \n",humid);
 
-      bsp_timer_delay(2000);
-    } 
-}
+//       bsp_timer_delay(2000);
+//     } 
+// }
 
 
-static void system_manage_test_button (void)
-{
-  drv_btn_update_state();
-}
+// static void system_manage_test_button (void)
+// {
+//   drv_btn_update_state();
+// }
 
 
 static void system_manage_test (void)
