742980d4d56926d485b2dafd4be63d43e741a275
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 13:05:50 +0700

    Init function system display idle

--- lib/src/system/system_display.c
+++ lib/src/system/system_display.c
@@ -1,6 +1,12 @@
 #include "system_display.h" 
+#include "image_128x64.h"
+
 
 esp_err_t system_display_idle (void)
 {
+  drv_sh1106_clear_screen();
+  bsp_timer_delay(10);
 
+  drv_sh1106_display_image(image_logo_uit);
+  bsp_timer_delay(10);
 }
