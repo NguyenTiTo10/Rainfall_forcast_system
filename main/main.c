81ed67e678f3a7488f91e5e12d7f41735c2b4ab0
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 10:36:45 +0700

    Add function config gpio into main

--- main/main.c
+++ main/main.c
@@ -17,7 +17,11 @@
 #define     MID_BTN_GPIO        GPIO_NUM_26
 #define     RIGHT_BTN_GPIO      GPIO_NUM_27
 
+static const char *TAG = "Button";
+
 static esp_err_t i2c_master_init(void);
+static esp_err_t config_gpio (void);
+
 
 static esp_err_t i2c_master_init(void) 
 {
@@ -33,6 +37,42 @@ static esp_err_t i2c_master_init(void)
     i2c_param_config(I2C_MASTER_NUM, &conf);                                      
     return i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);          
 }
+
+
+static esp_err_t config_gpio (void)
+{
+    gpio_config_t io_conf = 
+    {
+        .mode = GPIO_MODE_INPUT,               // Set as input
+        .pull_up_en = GPIO_PULLUP_DISABLE,     // Disable pull-up
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,  // Enable pull-down
+        .intr_type = GPIO_INTR_POSEDGE         // Trigger interrupt on rising edge
+    };
+    
+
+    // Configuring GPIO32
+    io_conf.pin_bit_mask = (1ULL << MAIN_BTN_GPIO);  // Select GPIO 32
+    esp_err_t err = gpio_config(&io_conf);
+    if (err != ESP_OK) return err;
+
+    // Configuring GPIO25
+    io_conf.pin_bit_mask = (1ULL << LEFT_BTN_GPIO);  // Select GPIO 25
+    err = gpio_config(&io_conf);
+    if (err != ESP_OK) 
+        return err;
+
+    // Configuring GPIO26
+    io_conf.pin_bit_mask = (1ULL << MID_BTN_GPIO);  // Select GPIO 26
+    err = gpio_config(&io_conf);
+    if (err != ESP_OK) 
+        return err;
+
+    // Configuring GPIO27
+    io_conf.pin_bit_mask = (1ULL << RIGHT_BTN_GPIO);  // Select GPIO 27
+    err = gpio_config(&io_conf);
+        return err;
+}
+
 
 void app_main(void) 
 {
