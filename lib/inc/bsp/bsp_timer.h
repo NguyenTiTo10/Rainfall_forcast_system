33faa60dd62c5e34eac33f252a6304009d69d245
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 10:13:46 +0700

    Add function bsp_timer_get_time

--- lib/inc/bsp/bsp_timer.h
+++ lib/inc/bsp/bsp_timer.h
@@ -8,8 +8,9 @@
 #ifndef BSP_TIMER_H
 #define BSP_TIMER_H
 
-void bsp_delay (uint32_t time);
-void bsp_timer_ets_delay_us (uint32_t time_us);
+void      bsp_delay                (uint32_t time);
+void      bsp_timer_ets_delay_us   (uint32_t time_us);
+int64_t   bsp_timer_get_time       (void);
 
 
 #endif
