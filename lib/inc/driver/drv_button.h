ac404d55683b92d68bceaeca4b774d0f3a3e8878
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 10:34:19 +0700

    Add files header and source drvier button

---
+++ lib/inc/driver/drv_button.h
@@ -0,0 +1,35 @@
+#ifndef DRV_BUTTON_H
+#define DRV_BUTTON_H
+
+#include "driver/gpio.h"
+#include "esp_system.h"  // Include this for IRAM_ATTR
+#include "esp_log.h"
+#include "bsp_gpio.h"
+#include "bsp_timer.h"
+
+#define MAIN_BTN_GPIO     GPIO_NUM_32       
+#define LEFT_BTN_GPIO     GPIO_NUM_25
+#define MID_BTN_GPIO      GPIO_NUM_26
+#define RIGHT_BTN_GPIO    GPIO_NUM_27
+
+
+typedef enum
+{
+  NON_BTN_PRESSED = 0,
+  MAIN_BTN_PRESSED,
+  LEFT_BTN_PRESSED,
+  MID_BTN_PRESSED,
+  RIGHT_BTN_PRESSED
+} drv_btn_type_t;
+
+typedef enum
+{
+  NO_PRESS = 0,
+  PRESSED
+} drv_btn_press_state_t;
+
+
+drv_btn_type_t drv_btn_detect_press (void);
+void drv_btn_update_state (void);
+
+#endif 
---
+++ lib/src/driver/drv_button.c
@@ -0,0 +1,73 @@
+#include "drv_button.h"
+
+#define TAG "Button ISR"          // Define a tag for logging
+#define DEBOUNCE_DELAY_MS 50     // Debounce delay in milliseconds
+
+static  uint32_t           last_debounce_time = 0;               // Last debounce time
+static  uint32_t           current_time;
+static  uint32_t           elasped_time;
+
+static  uint8_t             count = 1;
+
+
+drv_btn_type_t drv_btn_detect_press (void)
+{
+  if (!bsp_get_isr_flag())
+    return NON_BTN_PRESSED;
+
+    // Count the time to check whether debounce or not
+  current_time = bsp_timer_get_time() / 1000;
+  elasped_time = current_time - last_debounce_time;
+  if (elasped_time <= DEBOUNCE_DELAY_MS)
+    return NON_BTN_PRESSED;
+
+  bsp_set_isr_flag (false);
+  last_debounce_time = current_time;
+
+  if (bsp_gpio_read_pin(MAIN_BTN_GPIO) == 1)
+    return MAIN_BTN_PRESSED;
+  
+  if (bsp_gpio_read_pin(LEFT_BTN_GPIO) == 1)
+    return LEFT_BTN_PRESSED;
+
+  if (bsp_gpio_read_pin(MID_BTN_GPIO) == 1)
+    return MID_BTN_PRESSED;
+
+  if (bsp_gpio_read_pin(RIGHT_BTN_GPIO) == 1)
+    return RIGHT_BTN_PRESSED;
+
+  else
+    return -1;
+}
+
+
+
+void drv_btn_update_state (void)
+{
+  drv_btn_type_t btn_state = drv_btn_detect_press();
+
+  // Logging the button press
+  switch (btn_state) 
+  {
+    case MAIN_BTN_PRESSED:
+      ESP_EARLY_LOGI(TAG, "Count: %d - Main Button Pressed!", count);
+      break;
+    case LEFT_BTN_PRESSED:
+      ESP_EARLY_LOGI(TAG, "Count: %d - Left Button Pressed!", count);
+      break;
+    case MID_BTN_PRESSED:
+      ESP_EARLY_LOGI(TAG, "Count: %d - Mid Button Pressed!", count);
+      break;
+    case RIGHT_BTN_PRESSED:
+      ESP_EARLY_LOGI(TAG, "Count: %d - Right Button Pressed!", count);
+      break;
+    default:
+      count --;
+      break;
+  }
+  
+  count ++;
+}
+
+
+  
