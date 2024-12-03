5cc86750aa18f45c84b976b9d8f265869dc8fd63
nguyentito10 <tientoan91002003@gmail.com>
2024-12-03 13:10:51 +0700

    Update: function display idle and boot state

--- lib/src/system/system_display.c
+++ lib/src/system/system_display.c
@@ -7,8 +7,9 @@ esp_err_t system_display_idle (void)
   drv_sh1106_clear_screen();
   bsp_timer_delay(10);
 
-  drv_sh1106_display_image(image_logo_uit);
-  bsp_timer_delay(10);
+  // Start let's dht11 read data
+
+  // Start button
 
   return ESP_OK;
 }
@@ -16,5 +17,6 @@ esp_err_t system_display_idle (void)
 
 esp_err_t system_display_boot (void)
 {
-
+  drv_sh1106_display_image(image_logo_uit);
+  bsp_timer_delay(10);
 }
