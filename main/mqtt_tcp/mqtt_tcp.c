c4c1e5b8d1fb70ef8530beb7c2e5935d6e25bc0b
nguyentito10 <tientoan91002003@gmail.com>
2024-12-05 00:30:19 +0700

    Update: Created function esp_log_connect

--- main/mqtt_tcp/mqtt_tcp.c
+++ main/mqtt_tcp/mqtt_tcp.c
@@ -150,12 +150,27 @@ static void mqtt_app_start(void)
     esp_mqtt_client_start(client);
 }
 
-void mqtt_main(void)
+
+void esp_log_connect (void)
 {
     ESP_LOGI(TAG, "[APP] Startup..");
     ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
     ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
+    esp_log_level_set("mqtt_example", ESP_LOG_VERBOSE);
+    esp_log_level_set("transport_base", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("transport", ESP_LOG_VERBOSE);
+    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+}
+
+
+void mqtt_main(void)
+{
+    // esp_log_connect();
+    
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
