c20bc43fc849edb01c6021249ea9a13f4458d698
nguyentito10 <tientoan91002003@gmail.com>
2024-12-05 23:12:29 +0700

    Refactor: create funciton display ha tinh

--- lib/inc/system/system_display.h
+++ lib/inc/system/system_display.h
@@ -24,17 +24,17 @@ typedef struct
 
 
 
-esp_err_t system_display_idle       (void);
+esp_err_t system_display_idle             (void);
 
-esp_err_t system_display_boot       (void);
+esp_err_t system_display_boot             (void);
 
-esp_err_t system_display_screen_1   (system_data_display_t data);       // Could be some parameter later
+esp_err_t system_display_hatinh_sensor    (system_data_display_t data);       
 
-esp_err_t system_display_screen_2   (system_data_display_t data);       // Could be some parameter later
+esp_err_t system_display_hatinh_rain      (system_data_display_t data);       
 
-esp_err_t system_display_offline    (void);  
+esp_err_t system_display_offline          (void);  
 
-esp_err_t system_display_test       (void);
+esp_err_t system_display_test             (void);
 
 
 #endif
