b3a1e1e63649b05c647649d73ec0d5450b4bb46a
nguyentito10 <tientoan91002003@gmail.com>
2024-12-05 13:23:45 +0700

    Update: Apply function set_mqtt_event_data and get bsp_mqtt_set_data_flag

--- main/mqtt_tcp/bsp_mqtt_tcp.c
+++ main/mqtt_tcp/bsp_mqtt_tcp.c
@@ -17,6 +17,8 @@ bool mqtt_event_data_flag = false;
 static const char *TAG = "mqtt_example";
 
 void set_mqtt_event_data (void);
+bool bsp_mqtt_get_data_flag (void);
+void bsp_mqtt_set_data_flag (bool status);
 
 
 static void log_error_if_nonzero(const char *message, int error_code)
@@ -71,16 +73,16 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         case MQTT_EVENT_DATA:
             ESP_LOGI(TAG, "MQTT_EVENT_DATA");
 
-            mqtt_event_data_flag = true;
-            printf("Flag value: %s\n", mqtt_event_data_flag ? "true" : "false");
+            bsp_mqtt_set_data_flag(true);
+            printf("Flag value: %s\n", bsp_mqtt_get_data_flag() ? "true" : "false");
 
             set_mqtt_event_data ();
 
             printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
             printf("DATA=%.*s\r\n", event->data_len, event->data);
 
-            mqtt_event_data_flag = false;
-            printf("Flag value: %s\n", mqtt_event_data_flag ? "true" : "false");
+            bsp_mqtt_set_data_flag(false);
+            printf("Flag value: %s\n", bsp_mqtt_get_data_flag() ? "true" : "false");
             break;
 
         case MQTT_EVENT_ERROR:
