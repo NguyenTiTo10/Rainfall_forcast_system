17a836a2b0f6ae8c4ab7e927c41a438800715ac6
nguyentito10 <tientoan91002003@gmail.com>
2024-12-09 10:44:36 +0700

    Update: code header system data

--- lib/inc/system/system_data.h
+++ lib/inc/system/system_data.h
@@ -1,4 +1,16 @@
 #ifndef SYSTEM_DATA_H
 #define SYSTEM_DATA_H
 
+#include "drv_bmp180.h"
+#include "drv_dht11.h"
+#include "drv_button.h"
+
+typedef struct 
+{
+  float temp;
+  float humid;
+  float pressure;
+} sys_data_sensor_update_t;
+
+
 #endif
