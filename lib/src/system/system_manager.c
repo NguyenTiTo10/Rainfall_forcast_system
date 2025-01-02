8f5de790a4ee0158fe636d61a7883211fe58b54d
nguyentito10 <tientoan91002003@gmail.com>
2025-01-02 09:55:32 +0700

    Update: Completed static var of sensor data and update rain

--- lib/src/system/system_manager.c
+++ lib/src/system/system_manager.c
@@ -12,6 +12,12 @@
 static int64_t current_time_update_sensor;
 static int64_t last_time_update_sensor;  
 
+static int64_t current_time_send_sensor;
+static int64_t last_time_send_sensor; 
+
+static int64_t current_time_update_rain;
+static int64_t last_time_update_rain; 
+
 static void system_manage_init_data_display(void);
 
 
